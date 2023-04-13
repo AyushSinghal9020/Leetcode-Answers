0be99d20465ad29ed774166b2a770f09b0643eca
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-13 20:51:12 +0530

    Update Python Source Code.py

--- Hard /164 Maximum Gap/Python Source Code.py
+++ Hard /164 Maximum Gap/Python Source Code.py
@@ -1,4 +1,6 @@
 nums = [3,6,9,1]
 nums.sort()
+sum = 0
 for i in range(len(nums) - 1):
-    print(abs(nums[i] - nums[i + 1]))
+    if abs(nums[i] - nums[i + 1]) > sum :
+        sum = abs(nums[i] - nums[i + 1])
