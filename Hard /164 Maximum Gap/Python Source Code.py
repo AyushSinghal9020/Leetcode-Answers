f85ddf52969964fe64cecbb278878e315e2778bd
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-04-13 20:51:53 +0530

    Update Python Source Code.py

--- Hard /164 Maximum Gap/Python Source Code.py
+++ Hard /164 Maximum Gap/Python Source Code.py
@@ -1,7 +1,10 @@
-nums = [3,6,9,1]
-nums.sort()
-sum = 0
-if len(nums) < 2: print(0)
-for i in range(len(nums) - 1):
-    if abs(nums[i] - nums[i + 1]) > sum :
-        sum = abs(nums[i] - nums[i + 1])
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        nums.sort()
+        sum = 0
+        if len(nums) < 2: return sum
+        else :
+            for i in range(len(nums) - 1):
+                if abs(nums[i] - nums[i + 1]) > sum :
+                    sum = abs(nums[i] - nums[i + 1])
+            return sum
