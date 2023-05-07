aedba5a2ded30ce354674acae4c1845f39770b45
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-05-07 18:39:54 +0530

    Update Python Source Code.py

--- Easy/88 Merge Sorted Array/Python Source Code.py
+++ Easy/88 Merge Sorted Array/Python Source Code.py
@@ -3,9 +3,9 @@ m = 3
 nums2 = [2,5,6]
 n = 3
 nums1 = nums1[:len(nums2)] 
- 
-for i in nums2:
+ for i in nums2:
     for j in range(len(nums1)):
         if i < nums1[j]:
             nums1.insert(j , i)
+            nums2.pop(0)
 nums1
