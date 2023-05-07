4a5a37cf844d9d5c984f9f4df228fd7545ba9df8
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-05-07 18:23:36 +0530

    Update Python Source Code.py

--- Easy/88 Merge Sorted Array/Python Source Code.py
+++ Easy/88 Merge Sorted Array/Python Source Code.py
@@ -3,3 +3,9 @@ m = 3
 nums2 = [2,5,6]
 n = 3
 nums1 = nums1[:len(nums2)] 
+
+for i in nums2:
+    for j in range(len(nums1)):
+        if i < nums1[j]:
+            pass
+        # We need to do something here major !!!
