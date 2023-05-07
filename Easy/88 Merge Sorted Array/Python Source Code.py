ee2a8423a11619f5e0036e29b627f97042084ec0
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-05-07 18:40:09 +0530

    Update Python Source Code.py

--- Easy/88 Merge Sorted Array/Python Source Code.py
+++ Easy/88 Merge Sorted Array/Python Source Code.py
@@ -1,11 +1,11 @@
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [2,5,6]
-n = 3
-nums1 = nums1[:len(nums2)] 
- for i in nums2:
-    for j in range(len(nums1)):
-        if i < nums1[j]:
-            nums1.insert(j , i)
-            nums2.pop(0)
-nums1
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        nums1 = nums1[:len(nums2)]
+        for i in nums2:
+            for j in range(len(nums1)):
+                if i < nums1[j]:
+                    nums1.insert(j , i)
+                    nums2.pop(0)
+                    break
+        nums1 = nums1 + nums2
+        return nums1
