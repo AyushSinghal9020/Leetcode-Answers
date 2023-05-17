b1af0eb39961787c2e746bfa02e22850374b3ea0
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-05-17 17:55:58 +0530

    Create Python Source Code.py

---
+++ Easy/1 Two Sum/Python Source Code.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+    def twoSum(self, s, t):
+        
+        for i in range(0 , len(s)):
+            
+            for j in range(0, len(s)):
+                
+                if i != j:
+                    
+                    if s[i] + s[j] == t:
+                        
+                        if i in o:
+                            
+                            continue
+                        
+                        else :
+                            
+                            o.append(i)
+                        
+                        if j in o:
+                            
+                            continue
+                        
+                        else : 
+                            
+                            o.append(j)
+        
+        return o
