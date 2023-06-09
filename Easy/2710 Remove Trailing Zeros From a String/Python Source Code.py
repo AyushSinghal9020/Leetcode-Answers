c815bba418a38eb83337586f447a3d9a8e941659
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-06-09 17:15:50 +0530

    Create Python Source Code.py

---
+++ Easy/2710 Remove Trailing Zeros From a String/Python Source Code.py
@@ -0,0 +1,9 @@
+class Solution:
+    
+    def removeTrailingZeros(self, num: str) -> str:
+        
+        while num[-1] == "0":
+            
+            num = num[:-1]
+        
+        return num
