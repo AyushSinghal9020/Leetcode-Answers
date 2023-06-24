b90d58e41644fca0f8d21d734e5c93ecc4aeff74
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-06-24 12:57:14 +0530

    Create Python Source Code.py

---
+++ Easy/2670 Find the Distinct Difference Array/Python Source Code.py
@@ -0,0 +1,12 @@
+class Solution:
+    
+    def distinctDifferenceArray(self, nums: list[int]) -> list[int]:
+        
+        out = [
+            
+            len(set(nums[: i + 1])) - len(set(nums[i + 1 : ]))
+            for i in range(len(nums))
+            
+        ]
+        
+        return out
