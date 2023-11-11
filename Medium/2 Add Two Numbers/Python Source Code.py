7503d040a5e6fce06d9aa9209812234435b9b621
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-11-11 13:15:32 +0530

    Update Python Source Code.py

--- Medium/2 Add Two Numbers/Python Source Code.py
+++ Medium/2 Add Two Numbers/Python Source Code.py
@@ -1,44 +1,67 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     
-    def addTwoNumbers(self, l1, l2):
-      
-        list_node_a = l1
-        list_node_b = l2
-        
-        temp_node = list_node_c = ListNode(0)
-        
-        while list_node_a or list_node_b:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        def iter_link(head):
+
+            value = []
+            sample_head = head
+
+            while sample_head :
+
+                value.append(sample_head.val)
+                sample_head = sample_head.next
+
+            return value 
+
+        val_1 = iter_link(l1)
+        val_2 = iter_link(l2)
+
+        if len(val_1) > len(val_2) : val_2 += [0] * (len(val_1) - len(val_2))
+        elif len(val_2) > len(val_1) : val_1 += [0] * (len(val_2) - len(val_1))
+
+        val_1 += [0]
+        val_2 += [0]
+
+        carry = None
+        added = []
+
+        print(val_1 , val_2)
+
+        for index in range(len(val_1)) : 
+
+            value = val_1[index] + val_2[index]
+
+            if carry : 
+                value += int(carry)
+                carry = None
+
+            value = str(value) 
+
+            if len(value) > 1 : 
+
+                carry = value[:-1]
+
+            added.append(value[-1])
+
+        if added[-1] == '0' : added.pop(-1)
+
+        dummy_head = ListNode(0)
+        tail = dummy_head
+
+        for val in added:
+
+            new_node = ListNode(int(val))
             
-            a = b = 0  
-            
-            if not list_node_a:
-                
-                list_node_a = ListNode(0)
-            
-            a = list_node_a.val
-            
-            if not list_node_b:
-                
-                list_node_b = ListNode(0)
-            
-            b = list_node_b.val
-            
-            temp_add = a + b + temp_node.val
-            temp_node.val = temp_add % 10
-            
-            if temp_add // 10 == 1:
-                 
-                temp_node.next = ListNode(1)
-            
-            else:
-                
-                if list_node_a.next or list_node_b.next:
-                    
-                    temp_node.next = ListNode(0)  
-                
-            list_node_a = list_node_a.next
-            list_node_b = list_node_b.next
-            
-            temp_node = temp_node.next
-        
-        return list_node_c
+            tail.next = new_node
+            tail = tail.next
+
+        result = dummy_head.next 
+        dummy_head.next = None
+
+        return result
