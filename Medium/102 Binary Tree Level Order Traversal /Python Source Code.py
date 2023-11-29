85cff2272f45d2fe5eda8dff580f510e69eb95f4
Ayush Singhal <91676003+AyushSinghal9020@users.noreply.github.com>
2023-11-29 16:37:16 +0530

    Create Python Source Code.py

---
+++ Medium/102 Binary Tree Level Order Traversal /Python Source Code.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+        rt = {}
+
+        def recur(root , level) : 
+
+            if root : 
+
+                recur(root.left , level + 1)
+
+                if level in rt.keys() : rt[level].append(root.val)
+                else : rt[level] = [root.val]
+
+                recur(root.right , level + 1)
+
+        recur(root , 0)
+
+        ret = sorted(rt.keys())
+        rt = [
+            rt[val]
+            for val 
+            in ret
+        ]
+
+        return rt
+        
